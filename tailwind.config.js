a83963af667891d8f25ce077ba7e72197d4a0a9c
isha <ishaa0182@gmail.com>
2025-09-09 13:15:24 +0530

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 export default {
-  content: ['./src/**/*.{js,jsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
